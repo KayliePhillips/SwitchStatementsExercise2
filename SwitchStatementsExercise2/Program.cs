f5c112d6b4151f2848ce7bcb63f40df69885c322
Kaylie Phillips <kaylie.travel@gmail.com>
2022-09-14 17:00:04 -0500

    initial commit

---
+++ SwitchStatementsExercise2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Ask user for their favorite school subject.
+
+Console.WriteLine("What is your favorite school subject?");
+var userInput = Console.ReadLine();
+
+switch (userInput.ToLower())
+{
+	case "math":
+		Console.WriteLine("Math is lots of fun!");
+		break;
+	case "english":
+		Console.WriteLine("English is a good thing to master.");
+		break ;
+	case "science":
+		Console.WriteLine("Science can be challenging but fun.");
+		break;
+	case "PE": 
+	case "gym":
+		Console.WriteLine("Who doesn't love gym class");
+		break;
+	default:
+		Console.WriteLine("School is fun!");
+		break;
+}
